231c3ecd2608727bd929887d7bca1d0fb352f344
Eduardo <eduardocandaten10@gmail.com>
2024-09-27 10:49:18 -0300

    javascript

---
+++ js/smooth-scroll.js
@@ -0,0 +1,16 @@
+const links = document.querySelectorAll('.header-menu a[href^="#"]');
+
+links.forEach((link) => {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const targetSection = document.querySelector(this.getAttribute('href'));
+    const topPosition =
+      targetSection.getBoundingClientRect().top + window.scrollY - 20;
+
+    window.scrollTo({
+      top: topPosition,
+      behavior: 'smooth',
+    });
+  });
+});
